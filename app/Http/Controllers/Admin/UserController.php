b757e45d5e102da03c77a3b60036800ce7f8d9e7
Gustavo Kruger <favolin2014@gmail.com>
2026-03-09 09:46:49 -0300

    create users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,4 +14,15 @@ class UserController extends Controller
 
         return view('admin.users.index', compact('users'));
     }
+
+    public function create(){
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request){
+        
+        User::create($request->all());
+
+        return redirect()->route('users.index');
+    }
 }
