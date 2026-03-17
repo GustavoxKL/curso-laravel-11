70ccf0c1a9219e71f5c21e25b0c07fc4b8690bdf
Gustavo Kruger <favolin2014@gmail.com>
2026-03-17 09:56:01 -0300

    componentes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('Title') - Especializa TI</title>
+
+    @vite(('resources/css/app.css', 'resources/js/app.js'))
 </head>
 <body>
     <header>Header default</header>
-    
     @yield('content')
-
     <footer>Footer </footer>
 </body>
 </html>
