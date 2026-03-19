2cbe3283f6d9ae4f3b9d4e380536ebcecc90ed63
Gustavo Kruger <favolin2014@gmail.com>
2026-03-19 09:48:29 -0300

    validações avançadas de edição

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -22,7 +23,7 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request){
         
-        User::create($request->all());
+        User::create($request->validated());
 
         return redirect()
             ->route('users.index')
@@ -40,16 +41,16 @@ class UserController extends Controller
         return view('admin.users.edit', compact('user'));
     }
 
-    public function update(Request $request, string $id){
+    public function update(UpdateUserRequest $request, string $id){
         
         if (!$user = User::find($id)) {
             return back()->with('message', 'Usuário não encontrado');
         }
-
-        $user->update($request->only([
-            'name',
-            'email',
-        ]));
+        $data = $request->only('name', 'email');
+        if($request->passwod){
+            $data['password'] = bcrypt($request->passwod);
+        }
+        $user->update($data);
 
         return redirect()
             ->route('users.index')
